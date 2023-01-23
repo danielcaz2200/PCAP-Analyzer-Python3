6597d9b7571c956c47c80544415765b05af32dd9
Daniel Cazarez <60197297+danielcaz2200@users.noreply.github.com>
2023-01-22 20:41:40 -0800

    cleaned up code, absolute paths, argparse

--- main.py
+++ main.py
@@ -1,40 +1,63 @@
 # Title: Network traffic maps visualizer
 # Author: Daniel Cazarez
-# CPSC 440 Project - Prof. Heckathorn
 
 import pcap_inspector
 import os
 import sys
+import argparse as ap
 
 pcap_dir = 'pcap_files'
-
-
-def main(filenames=None, summarize=False, count=False, kml=False):
-    if count:
-        pcap_inspector.count_pkts(filenames)
-    if summarize:
-        pcap_inspector.summarize_files(filenames)
-    if kml:
-        pcap_inspector.pcap_to_kml()
-
+mapdata_dir = 'mapdata'
 
 # Run script
 if __name__ == '__main__':
+    cwd = os.getcwd()
+
+    # Get absolute paths
+    pcap_dir_abs = os.path.join(cwd, pcap_dir)
+    mapdata_dir_abs = os.path.join(cwd, mapdata_dir)
+
+    CHECK_FOLDER = os.path.isdir(pcap_dir_abs)
+    if not CHECK_FOLDER:
+        os.makedirs(pcap_dir_abs)
+
+    CHECK_FOLDER = os.path.isdir(mapdata_dir_abs)
+    if not CHECK_FOLDER:
+        os.makedirs(mapdata_dir_abs)
 
     filenames = []
-    for filename in os.listdir(pcap_dir):
+    for filename in os.listdir(pcap_dir_abs):
         # Concatenated path components, entire string
-        file_in_dir = os.path.join(pcap_dir, filename)
+        file_in_dir = os.path.join(pcap_dir_abs, filename)
         filenames.append(file_in_dir)
 
-    if len(sys.argv) > 1:
-        if '--count' in sys.argv:
-            main(filenames=filenames, count=True)
-        if '--summarize' in sys.argv:
-            main(filenames=filenames, summarize=True)
-        if '--kml' in sys.argv:
-            main(kml=True)
+    parser = ap.ArgumentParser()
 
-    else:
+    parser.add_argument('--count', action='store_true',
+                        help='Counts packets in a Wireshark pcap file')
+    parser.add_argument('--summarize', action='store_true',
+                        help='Gives line by line summary of packets in a pcap file')
+    parser.add_argument('--kml', action='store_true',
+                        help='Creates formatted kml file from a pcap file in the pcap_files dir')
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
         print('ERROR: You must supply at least one argument.\n')
-        print('Usage: "python3 main.py --arg" where arg = --count, --summarize or --kml')
+        parser.print_help()
+        sys.exit()
+    elif not filenames:
+        print(
+            f"ERROR: You must import at least one wireshark .pcap capture file to: {pcap_dir_abs}"
+        )
+        sys.exit()
+    elif args.count:
+        pcap_inspector.count_pkts(filenames=filenames)
+    elif args.summarize:
+        pcap_inspector.summarize_files(filenames=filenames)
+    elif args.kml:
+        pcap_inspector.pcap_to_kml()
+    else:
+        print('ERROR: You must supply a valid argument to the script.\n')
+        parser.print_help()
+        sys.exit()
